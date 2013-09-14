d5a303666a067354975af16352db42277602ebf1
Ian <Ian.c.kane@wmich.edu>
2013-09-14 17:23:21 -0400

    Changed some datatypes in Htree.hs

--- HTree.hs
+++ HTree.hs
@@ -1,79 +1,107 @@
-module HTree
-    (HTree(..)
-    ,Symbol
-    ,Weight
-    ,Frequency
-    ,SymbolSet
-    ,makeLeaf
-    ,isLeaf
-    ,makeCodeTree) where
-      
+import Data.List
+import Data.Set as Set  
+import Test.HUnit
+import Test.QuickCheck
+
+
+--Huffman Codes allows data to be represented as sequences of ones and zeros.
+--The encoding mechanism uses variable length codes to represent textual data
+--taking advantange of the fact that some symbols appear more frequently than
+--others.  The most frequent occuring symbols are encoded to bit sequences of
+--the shortest length.
+  
+--One difficulty of variable length code is, when reading a sequence of bits,
+--recognizing the end of one symbol and the beggining of the next.  One
+--solution would be to use a seperator symbol that signifies the boundries.
+--Another solution is to design the encoding scheme such that no complete
+--code for any symbol is the beggining (or prefix) of the code for any other
+--symbol.
+
+--This module implements David Huffmans encoding scheme called Huffman codes.
+--A Huffman Code can be represented as a binary tree whos leaves are the
+--symbols that are encoded.  At each nonleaf node there is the set composed
+--of all the symbols in the leaves that lie below it.  Additionally each
+--leaf node is assigned the weight of its relative frequency and each non -
+--leaf node is has a weight that is the sum of the weights of all the leaves
+--lying below it.
+  
 type Symbol = Char
-type Weight = Integer
-type Frequency = (Symbol, Weight)
-type SymbolSet = [Symbol]
+              
+type Weight = Int
+              
+type Frequency = (Int, Symbol)
+
+type WeightedAlphabet = Set Frequency
+
+data HuffmanTree = HLeaf {frequency :: Frequency} 
+                 | Branch {
+                    symbolSet :: WeightedAlphabet
+                   ,left              :: HuffmanTree
+                   ,right             :: HuffmanTree
+                   } deriving (Show,Read,Eq,Ord)
+  
+
+encodeText :: [Symbol] -> HuffmanTree
+encodeText = undefined
+
+freqmap' :: [Symbol] -> [Frequency]
+freqmap' = undefined
+
+--Does it make sense to accept a list of Maybe symbols?
+--If there are symbols, then there are frequencies.
+frequencyMap :: [Symbol] -> [Frequency]
+frequencyMap str =
+  let counts = Data.List.map length (group . sort $ str)
+      symbols = Data.List.map head (group . sort $ str)
+  in zip counts symbols
+
+frequencyForrest :: [Frequency] -> [HuffmanTree]
+frequencyForrest  = Data.List.map HLeaf
+
+codeTree :: HuffmanTree -> HuffmanTree -> HuffmanTree
+codeTree (HLeaf f1) (HLeaf f2) =
+    Branch {
+      symbolSet = Set.fromList (f1:f2:[])
+      ,left = HLeaf f1
+      ,right = HLeaf f2
+      }
+  
+codeTree leftT rightT = 
+     Branch {
+       symbolSet = Set.union (symbolSet leftT) (symbolSet rightT) 
+       , left = leftT
+       , right = rightT
+       }
+
+weight :: HuffmanTree -> Weight
+weight tree = case tree of
+  Branch ss l r ->  _weight ss 
+  HLeaf f       ->  fst f
+
+_weight :: WeightedAlphabet -> Weight
+_weight = Set.fold (\freq count -> (fst freq) + count) 0
+  
+
+mergeTrees :: Set HuffmanTree -> HuffmanTree
+mergeTrees ts = undefined
+-- Set.minView ts >>=
+-- (t2, ts_pp) <- Set.minView ts_p
+-- return Set.insert (codeTree t1 t2) ts_pp
 
 
-data HTree = Leaf Frequency
-           | Branch HTree HTree Weight SymbolSet
-             deriving (Show )
+              
+      --TESTS--
+testEmptyString = TestCase $ assertEqual
+                  "Emptry string returns empty list."
+                  []
+                  (frequencyMap "")
+             
+testString = "TESTSTRING" 
+forrest = frequencyForrest (frequencyMap testString)
+first2 = take 2 forrest
+h1 = head first2
+h2 = head (tail first2)
+     
 
-instance Eq HTree where
-  (Leaf x)         == (Leaf y)           = x == y
-  (Branch l r _ _) == (Branch l' r' _ _) = l == l' && r == r'
-  _                ==  _                 = False
-
-instance Ord HTree where
-    (Leaf _ ) <= (Branch _ _ _ _)         = True
-    (Leaf x)  <= (Leaf y)                 = (weight x <= weight y)
-    (Branch _ _ _ _) <= (Leaf _)          = False
-    (Branch _ _ w _) <= (Branch _ _ w' _) = (w <= w')
-                      
-weight       :: Frequency -> Weight                 
-weight (s,w) = w
-
-symbol       :: Frequency -> Symbol               
-symbol (s,w) = s
-
-makeLeaf     :: Frequency -> HTree
-makeLeaf f = Leaf f
-
-isLeaf (Leaf f) = True
-isLeaf _        = False
-
-symbolLeaf          :: HTree -> Symbol
-symbolLeaf (Leaf f) = symbol f
-symbolLeaf _        = undefined
-
-weightLeaf          :: HTree -> Weight
-weightLeaf (Leaf f) = weight f
-weightLeaf _        = undefined
-
-append       :: SymbolSet -> SymbolSet -> SymbolSet
-append x1 x2 = case null x1 of
-                 True -> x2
-                 False -> (head x1) : (append (tail x1) x2)
-
-leftBranch                  :: HTree -> HTree
-leftBranch (Branch l _ _ _) = l
-leftBranch _                = undefined
-
-rightBranch                  :: HTree -> HTree
-rightBranch (Branch _ r _ _) = r
-rightBranch _                = undefined
-
-symbols          :: HTree -> SymbolSet
-symbols (Leaf f)          = (symbol f):[]
-symbols (Branch l r _ ss) = ss
-
-weights          :: HTree -> Weight
-weights (Leaf f)         = weight f 
-weights (Branch _ _ w _) = w
-
-makeCodeTree :: HTree -> HTree -> HTree
-makeCodeTree left right = let union =  (symbols left) ++ (symbols right)
-                              compoundWeight = (weights left) + (weights right)
-                          in
-                            Branch left right compoundWeight union
-
-
+                  
+main = runTestTT $ TestList [ testEmptyString ]
--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Data.Tree as T
 import Data.Map as M
+import Data.List as L
 import HTree as HT
   
 type Count = Integer           
@@ -9,20 +10,10 @@ testcase :: String
 testcase = "TEST STRING"
 --Count the frequency an character occurs in a String.
 
---Given a character key and a OccurenceMap, add that key to the map.  If key 
---already exists, increment occurence count.
-addToMap   :: Symbol -> Map Symbol Count -> Map Symbol Count
-addToMap x = M.insertWith (+) x 1
-
-frequencyMap   :: [Symbol] -> Map Symbol Count
-frequencyMap s = Prelude.foldr addToMap M.empty s
-
-frequencyCount   :: (Num a, Ord k) => Map k a -> [(k,a)]
-frequencyCount m = M.assocs m
-
-makeForrest    :: [Frequency] -> [HTree]
-makeForrest fs = Prelude.map makeLeaf fs
-
 -- Given a list of Trees, Combine the two smallest using makeCodeTree,
 -- and place the resulting tree in its proper place in the list
+compound :: [HTree] -> [HTree]
+compound (x1:x2:xs) = L.insert (makeCodeTree x1 x2) xs
+         
 
+                 
