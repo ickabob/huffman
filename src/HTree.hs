89855641789f9b50ed9c1302d35cb9cb6d2c5a77
Ian <Ian.c.kane@wmich.edu>
2013-12-22 22:33:38 -0500

    removed toplevel main.hs, Frequency type synonym is now a first class data type. Added a Bit type.

--- src/HTree.hs
+++ src/HTree.hs
@@ -27,27 +27,32 @@ import Test.HUnit
 
   
 type Symbol = Char
-
+              
 type Count = Int
              
 type Weight = Int
               
-type Frequency = (Count, Symbol)
+type Message = [Symbol]
 
-type WeightedAlphabet = Set Frequency
+data Bit = One | Zero deriving (Show,Read,Eq)
 
-_weight :: WeightedAlphabet -> Weight
-_weight = Set.fold (\freq count -> (fst freq) + count) 0
-
-data HTree = Leaf { frequency :: (Int,Char) }
+type Encoding = [Bit]
+              
+data Frequency = Frequency { count :: Int, symbol :: Symbol}
+                 deriving (Show,Read,Eq)
+                          
+instance Ord Frequency where
+  compare f1 f2 = compare (count f1) (count f2)
+                 
+data HTree = Leaf { frequency :: Frequency }
            | Branch { weight :: Weight
              ,left :: HTree
              ,right :: HTree
              }deriving (Show,Read,Eq)
                        
 instance Ord HTree where
-  compare (Leaf (w, _)) (Leaf (w', _)) = compare w w'
-  compare (Branch w _ _) (Leaf (w', _)) = compare w w'
+  compare (Leaf f1) (Leaf f2) = compare f1 f2
+  compare (Branch w _ _) (Leaf f1) = compare w (count f1)
   compare (Branch w _ _) (Branch w' _ _) = compare w w'                 
 
 _partition :: Ord a =>  [a] -> [[a]]
@@ -59,43 +64,41 @@ _counts = Data.List.map length . _partition
 _symbols :: [Symbol] -> [Symbol]
 _symbols = Data.List.map head . _partition
 
-countSymbols :: String -> [(Int,Char)]           
-countSymbols str = zip counts symbols
+countSymbols :: String -> [Frequency]           
+countSymbols str = Data.List.map (\(c,s)->Frequency c s) $ zip counts symbols
    where counts = _counts str
          symbols = _symbols str
 
 type HForest = Set HTree
                
-symbolForrest :: String -> HForrest
-symbolForrest = Set.fromList $ Data.List.map Leaf . countSymbols
+symbolForest :: String -> [HTree]
+symbolForest  = Data.List.map Leaf . countSymbols 
+
 
 encodingTree :: HForest -> HTree
 encodingTree f 
   |Set.null f       = error "Cannot construct Huffman Tree form the empty set."
   | Set.size f == 2 = merged 
-  | Set.size f >= 2 = constructOptimal $ Set.insert merged modified'
+  | Set.size f >= 2 = encodingTree $ Set.insert merged modified'
   where
     (max1,modified) = Set.deleteFindMax f
     (max2,modified') = Set.deleteFindMax modified
     merged = merge2 max1 max2
 
 merge2 :: HTree -> HTree -> HTree
-merge2 (Leaf {frequency = f1} ) (Leaf {frequency = f2})
-  =  Branch { weight = (fst f1) + (fst f2)
-            , left = Leaf f1
-            , right = Leaf f2}
-  
-merge2 (Leaf {frequency = f1} ) (Branch {weight = w, left = l, right = r})
-  = Branch { weight =  (fst f1) + w
-           , left = Leaf f1
-           , right = Branch w l r}
-                                                                                 
-merge2 (Branch {weight = w, left = l, right = r}) (Leaf {frequency = f1})
-  = Branch { weight = (fst f1) + w
-           , right = Branch w l r
-           , left = Leaf f1}
-                                                                                
-merge2 (Branch {weight = w1, left = l1, right = r1}) (Branch {weight = w2, left = l2, right = r2})
-  = Branch { weight = w1 + w2
-           , left = Branch w1 l1 r1
-           , right = Branch w2 l2 r2}
+merge2 h1@(Leaf f1) h2@(Leaf f2) =
+  Branch { weight = count f1 + count f2, left = h1, right = h2}
+merge2 h1@(Leaf f1) h2@(Branch _ _ _) =
+  Branch { weight = count f1 + weight h2, left = h1, right = h2}
+merge2 h1@(Branch _ _ _) h2@(Leaf f1) =
+    Branch { weight = weight h1 + count f1, left = h1, right = h2}
+merge2 h1@(Branch _ _ _) h2@(Branch _ _ _) =
+    Branch { weight = weight h1 + weight h2, left = h1, right = h2}
+
+encode :: Message -> HTree -> [Bit]
+encode "" tree = []  
+encode mes tree = Data.List.concatMap (encodeSymbol $ tree) mes
+
+encodeSymbol :: HTree -> Symbol -> [Bit]
+encodeSymbol = undefined                
+
